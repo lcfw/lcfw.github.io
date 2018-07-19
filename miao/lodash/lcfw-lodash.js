5cb0e8257bfb17dfa0011c6057b98564294b9a05
lcfw <aaa@bbb.com>
2018-07-19 23:25:06 +0800

    commit

--- miao/lodash/lcfw-lodash.js
+++ miao/lodash/lcfw-lodash.js
@@ -1,5 +1,4 @@
 var lcfw = {
-  //创建元素的数组，将元素的长度分成组。如果数组不能均匀分割，则最终块将是剩余元素。
   chunk: function(array, size) {
     var result = []
     for (var start = 0, end = size; start < array.length; start += size, end += size) {
@@ -7,7 +6,7 @@ var lcfw = {
     }
     return result
   }
-  //创建一个移除所有FalSee值的数组。值Falk、NULL、0、“”、“未定义”和楠都是FalSee。
+
   compact: function(array) {
     var result = []
     for (var item of array) {
@@ -15,8 +14,9 @@ var lcfw = {
         result.push(item)
       }
     }
-    return item
+    return result
   }
+
   difference: function(array, values) {
     var result = []
     for (var item of array) {
